0cb491c156cf0346c9335396fc7de451482ef487
Luigi Minardi <lugminsinlag@gmail.com>
2022-02-16 21:36:00 +0100

    Solving param bug, the routes with params was in front of routes without it causing a 404

--- views/MovieRouter.js
+++ views/MovieRouter.js
@@ -7,14 +7,14 @@ router.get('/search', MovieController.getSearchMovies);
 
 router.get('/new', MovieController.getNewMovies);
 
-router.get('/:pk', MovieController.getMovieById);
-
-router.get('/:pk/reviews', MovieController.getReviewByMovieId);
-
 router.get('/latest', MovieController.getLatestMovie);
 
 router.get('/bests', MovieController.getBestMovies);
 
+router.get('/:pk', MovieController.getMovieById);
+
 router.get('/similar_to/:pk', MovieController.getSimilarMovies);
+
+router.get('/:pk/reviews', MovieController.getReviewByMovieId);
 
 module.exports = router;
