4349f21f5982bf96f8137d4063aeb11731fd9b83
Luigi Minardi <lugminsinlag@gmail.com>
2022-02-16 12:15:17 +0100

    Doing the search endpoint

--- controllers/MovieController.js
+++ controllers/MovieController.js
@@ -1,12 +1,16 @@
-const axios = require('axios');
+const { default: axios } = require('axios');
 
 const MovieController = {};
 
 MovieController.getSearchMovie = async (req, res) => {
+    
+    let search = req.query.q;
+
+    let key = '210d6a5dd3f16419ce349c9f1b200d6d';
+
     try {
-        //let result = await axios.get('')
-        // req.send(result)
-        req.send(console.log('test'));
+        let result = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=${search}&page=1&include_adult=false`)
+        res.send(result.data.results)
     } catch (error) {
         res.send(error);
     }
