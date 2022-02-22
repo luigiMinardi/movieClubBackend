9a639036d7c34908884f5cd7cfa5b5131cf9490d
Luigi Minardi <lugminsinlag@gmail.com>
2022-02-22 15:00:31 +0100

    fixing payment model association, changing from movie to user (the correct one)

--- models/payment.js
+++ models/payment.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      this.belongsTo(models.Movie, {
+      this.belongsTo(models.User, {
         foreignKey: 'usuarioId'
       });
     }
