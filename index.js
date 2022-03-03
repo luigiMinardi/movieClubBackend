368dd7b6a7b75350348a2d6fc2385150e66a2a9f
Luigi Minardi <lugminsinlag@gmail.com>
2022-03-03 20:18:12 +0100

    Setting port to be on .env

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const cors = require('cors');
 const express = require('express');
 const app = express();
 const router = require('./router');
-const PORT = 5500;
+const PORT = process.env.PORT || 3000;
 const db = require('./db.js');
 
 //Middleware
