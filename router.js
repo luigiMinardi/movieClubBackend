65c9169ce2949176fd042a30429b63bbf2b92e7b
Luigi Minardi <lugminsinlag@gmail.com>
2022-03-03 21:29:12 +0100

    Sending user to the docs if they acess the root endpoint

--- router.js
+++ router.js
@@ -5,6 +5,9 @@ const MovieDBRouter = require('./views/MovieDBRouter');
 const UserRouter = require('./views/UserRouter');
 const OrderRouter = require('./views/OrderRouter');
 
+router.use('/', (req, res) => {
+    res.redirect(303, 'https://github.com/luigiMinardi/movieClubBackend/wiki/Redirect')
+});
 router.use('/movies', MovieRouter);
 router.use('/movie-db', MovieDBRouter);
 router.use('/users', UserRouter);
