adaeb3a25a98d65f503cc27c7e1117bee8a5938b
Luigi Minardi <lugminsinlag@gmail.com>
2022-03-04 14:06:37 +0100

    Solving redirect bug, all the api was redirecting to the docs and not just the root

--- router.js
+++ router.js
@@ -5,12 +5,12 @@ const MovieDBRouter = require('./views/MovieDBRouter');
 const UserRouter = require('./views/UserRouter');
 const OrderRouter = require('./views/OrderRouter');
 
-router.use('/', (req, res) => {
-    res.redirect(303, 'https://github.com/luigiMinardi/movieClubBackend/wiki/Redirect')
-});
 router.use('/movies', MovieRouter);
 router.use('/movie-db', MovieDBRouter);
 router.use('/users', UserRouter);
 router.use('/orders', OrderRouter);
+router.use('/', (req, res) => {
+    res.redirect(303, 'https://github.com/luigiMinardi/movieClubBackend/wiki/Redirect')
+});
 
 module.exports = router;
